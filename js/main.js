6e9fae19a073107b64406ca52a785e7dc3cebb91
janellewong <janellewong112@gmail.com>
2023-03-29 20:36:06 -0700

    filtered/processed data

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let data, data_filtered;
+/**
+ * Load data from CSV file asynchronously and render charts
+ */
+d3.csv('data/movies-processed.csv')
+  .then(_data => {
+  data = _data;
+  // Convert columns to numerical values
+  data = data.map(d => {
+    Object.keys(d).forEach(attr => {
+      if (attr == 'Budget' || attr == 'Income') {
+        d[attr] = (d[attr] == 'Unknown' || d[attr] == null) ? null : +d[attr];
+      } else if (attr == 'Country_of_origin' || attr == 'Genre' || attr == 'Stars' || attr == "Directors") {
+        d[attr] = d[attr].split(',').map(s => s.trim());
+      } else if (attr != 'Certificate' && attr != 'Filming_location' && attr != 'Month' && attr != 'Title') {
+        d[attr] = +d[attr];
+      }
+    });
+    return d;
+  });
+
+  console.log(data);
+
+  data_filtered = data.filter(d => d.Budget !== null); // if we're using income instead of budget, just change it to income instead 
+
+  scatterplot = new ScatterPlot({parentElement: '#scatter-plot',}, dispatcher, data_filtered); // put the filtered data in since we don't want unknowns in the scatterplot
+  scatterplot.updateVis();
+
+  barchart = new BarChart({parentElement: '#bar-chart',}, dispatcher, data);
+  barchart.updateVis();
+
+  heatmap = new Heatmap({parentElement: '#lexis-chart',}, dispatcher, data);
+  heatmap.updateVis();
+
+  geographic = new Geographic({parentElement: '#geographic-map',}, dispatcher, data);
+  geographic.updateVis();
+});
