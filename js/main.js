f3ff832858ba3bbc76bf6942d7be8395a96d3493
rebeccachang <rebecca.chang1001@gmail.com>
2023-04-07 16:57:19 -0700

    add interactivity for heatmap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
+// Global filters
 let data, data_filtered;
 
 let BudgetFilterValues = {
   min: 10,
   max: 356000000
-}
+};
+
+// ID of movies selected in heatmap
+let selectedMovies = new Set();
+
+// certificates selected from barchart as string
+let selectedCertificates = new Set();
 
 const dispatcher = d3.dispatch(
     'barchartFiltersScatterPlot',
     'barchartFiltersHeatmap',
-    'heatmapFiltersBarchart',
-    'heatmapFiltersScatterPlot'
 
+    'heatmapFiltersBarchart',
+    'heatmapFiltersScatterAndBar',
+
+    'heatmapFiltersBudgetSlider',
+    'barchartFiltersBudgetSlider'
 );
 
 // TODO: Instantiate charts with global filter slider
@@ -43,18 +53,50 @@ d3.csv('data/movies-processed.csv')
   geographic = new Geographic({parentElement: '#geographic-map',}, data, dispatcher);
 
   d3.select('#slider1 #slider2').on('change', function() {
-    scatterplot.data = data
-    barchart.data = data
-    heatmap.data = data
-    geographic.data = data
-
-    scatterplot.updateVis()
-    barchart.updateVis()
-    heatmap.updateVis()
-    geographic.updateVis()
+    resetAllData(data);
+    updateAllVis();
   })
 
 }).catch(error => console.error(error));
+
+
+function updateAllVis() {
+  scatterplot.updateVis()
+  barchart.updateVis()
+  heatmap.updateVis()
+  geographic.updateVis()
+}
+
+// update scatterplot when movies are selected in heatmap
+dispatcher.on('heatmapFiltersScatterAndBar', () => {
+  if (!(selectedMovies.size === 0)) {
+    let newData = data.filter(movie => {
+      return selectedMovies.has(movie.ID)
+    })
+    scatterplot.data = newData
+    barchart.data = newData
+  }
+  else {
+    scatterplot.data = data;
+    barchart.data = data;
+  }
+  scatterplot.updateVis();
+  barchart.updateVis();
+});
+
+
+function resetAllData(_data) {
+  scatterplot.data = _data
+  barchart.data = _data
+  heatmap.data = _data
+  geographic.data = _data
+}
+
+function heatmapReset() {
+  selectedMovies.clear();
+  resetAllData(data);
+  updateAllVis();
+}
 
 
 /*
@@ -75,6 +117,14 @@ function controlSlider(fromSlider, toSlider) {
     BudgetFilterValues.max = toVal
     BudgetFilterValues.min = fromVal
   }
+
+  let updatedData = data.filter(movie => {
+    return (movie.Budget >= BudgetFilterValues.min && movie.Budget <= BudgetFilterValues.max)
+  })
+
+  resetAllData(updatedData);
+  updateAllVis();
+
 }
 
 const fromSlider = document.querySelector('#slider1');
